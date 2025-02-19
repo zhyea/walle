87f604427e0fd9f29fc953625d6d6773bd57a53f
zhyea <zhangrui@zhyea.com>
2025-02-19 23:23:33 +0800

    init

---
+++ src/main/java/org/chobit/walle/WalleAiApplication.java
@@ -0,0 +1,13 @@
+package org.chobit.walle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalleAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalleAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chobit/walle/WalleAiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chobit.walle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalleAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
