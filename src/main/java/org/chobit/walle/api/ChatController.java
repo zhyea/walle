d477d38329c9be57674d9934c5908da0e43b3891
zhyea <zhangrui@zhyea.com>
2025-02-25 23:06:32 +0800

    1. 调整配置-2

--- src/main/java/org/chobit/walle/api/ChatController.java
+++ src/main/java/org/chobit/walle/api/ChatController.java
@@ -1,9 +1,9 @@
 package org.chobit.walle.api;
 
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +15,15 @@ import java.util.Map;
 @RestController
 public class ChatController {
 
-	private final OpenAiChatModel chatModel;
+	private final ChatModel chatModel;
 
 	@Autowired
-	public ChatController(OpenAiChatModel chatModel) {
+	public ChatController(ChatModel chatModel) {
 		this.chatModel = chatModel;
 	}
 
 	@GetMapping("/ai/generate")
-	public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+	public Map<String, String> generate(@RequestParam(value = "message", defaultValue = "给我讲个笑话") String message) {
 		return Map.of("generation", this.chatModel.call(message));
 	}
 
